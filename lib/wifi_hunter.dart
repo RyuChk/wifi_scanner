50d1c2ff2aa30971b1a4c3eea97c9692334abb78
Thanwa Chokporntaveesuk <rewphg@172-0-0-149.lightspeed.brhmal.sbcglobal.net>
2024-03-22 16:35:27 +0700

    add timeout

--- lib/wifi_hunter.dart
+++ lib/wifi_hunter.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -10,7 +9,8 @@ class WiFiHunter {
 
   /// Start WiFi Scan
   static Future<WiFiHunterResult?> get huntWiFiNetworks async {
-    final Map<String, dynamic> networks = Map<String, dynamic>.from(await _channel.invokeMethod('huntWiFiNetworks'));
+    final Map<String, dynamic> networks = Map<String, dynamic>.from(
+        await _channel.invokeMethod('huntWiFiNetworks'));
 
     WiFiHunterResult result = WiFiHunterResult();
 
@@ -22,9 +22,47 @@ class WiFiHunter {
     List<int> channelWidths = List<int>.from(networks["CHANNELWIDTHS"]);
     List<int> timestamps = List<int>.from(networks["TIMESTAMPS"]);
 
-    for (var i = 0; i < ssids.length; i++)
-      result.results.add(WiFiHunterResultEntry(ssids[i], bssids[i], capabilities[i], frequencies[i], levels[i], channelWidths[i], timestamps[i]));
+    for (var i = 0; i < ssids.length; i++) {
+      result.results.add(WiFiHunterResultEntry(
+          ssids[i],
+          bssids[i],
+          capabilities[i],
+          frequencies[i],
+          levels[i],
+          channelWidths[i],
+          timestamps[i]));
+    }
 
     return result;
+  }
+
+  // Start WiFi scan with a timeout
+  static Future<WiFiHunterResult?> huntWiFiNetworksWithTimeout(
+      xinterval) async {
+    final Completer<WiFiHunterResult?> completer =
+    Completer<WiFiHunterResult?>();
+
+    // Start WiFi scan
+    final wifiScanFuture = huntWiFiNetworks;
+
+    // Set a timeout for the WiFi scan operation (e.g., 10 seconds)
+    final timeoutDuration = Duration(seconds: xinterval + 1);
+
+    // Create a delayed Future to handle the timeout
+    final timeoutFuture = Future.delayed(timeoutDuration, () {
+      completer.completeError(TimeoutException('WiFi scan timed out'));
+    });
+
+    // Wait for either the WiFi scan to complete or the timeout to occur
+    Future.any([wifiScanFuture, timeoutFuture]).then((result) {
+      // Handle the result (it could be either WiFi scan result or TimeoutException)
+      if (result is WiFiHunterResult) {
+        completer.complete(result);
+      }
+    }).catchError((error) {
+      completer.completeError(error);
+    });
+
+    return completer.future;
   }
 }
